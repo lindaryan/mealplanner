537fab81f6e0d06ff5cd2a08cb92707c41263ee9
Linda Ryan <linda.ryan@mygeorgian.ca>
2020-03-27 20:30:52 -0400

    basic layout/views

--- controllers/index.js
+++ controllers/index.js
@@ -2,8 +2,28 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
+router.get('/', (req, res, next) => {
   res.render('index', { title: 'Meal Planner' });
-});
+})
 
-module.exports = router;
+/* GET about page. */
+router.get('/about', (req, res, next) => {
+  res.render('about', { title: 'Meal Planner' });
+})
+
+// GET: calendar page
+router.get('/calendar', (req, res, next) => {
+  res.render('calendar')
+})
+
+// GET: login page
+router.get('/login', (req, res, next) => {
+  res.render('login')
+})
+
+// GET: register page
+router.get('/register', (req, res, next) => {
+  res.render('register')
+})
+
+module.exports = router

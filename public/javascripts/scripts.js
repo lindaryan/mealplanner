8fb25b2d2dc4cca7ea8cfe3263d67f40cf05ecb4
Linda Ryan <linda.ryan@mygeorgian.ca>
2020-04-06 02:24:33 -0400

    google auth

--- public/javascripts/scripts.js
+++ public/javascripts/scripts.js
@@ -1,17 +1,18 @@
-https://www.w3schools.com/howto/howto_js_navbar_sticky.asp
+    function confirmDelete() {
+        return confirm('Are you sure you want to delete this meal?')
+    }
 
-// const confirmDelete = () => { return confirm('Are you sure you want to delete this?') }
-
-function comparePasswords () {
+function comparePasswords() {
     var pw1 = document.getElementById('password').value
     var pw2 = document.getElementById('confirm').value
     var pwMsg = document.getElementById('pwMsg')
 
-    if(pw1 != pw2) {
-        pwMsg.innerText = "Passwords do not match!"
-        psMsg.className = "text-danger"
+    if (pw1 != pw2) {
+        pwMsg.innerText = "Passwords do not match"
+        pwMsg.className = "text-danger"
         return false
-    } else {
+    }
+    else {
         pwMsg.innerText = ""
         pwMsg.className = ""
         return true

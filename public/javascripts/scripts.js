c6e693be2a83b43f66dbccf351602fd9538c3529
Linda Ryan <linda.ryan@mygeorgian.ca>
2020-04-05 21:09:59 -0400

    fb login

--- public/javascripts/scripts.js
+++ public/javascripts/scripts.js
@@ -1,5 +1,22 @@
 https://www.w3schools.com/howto/howto_js_navbar_sticky.asp
 
+// const confirmDelete = () => { return confirm('Are you sure you want to delete this?') }
+
+function comparePasswords () {
+    var pw1 = document.getElementById('password').value
+    var pw2 = document.getElementById('confirm').value
+    var pwMsg = document.getElementById('pwMsg')
+
+    if(pw1 != pw2) {
+        pwMsg.innerText = "Passwords do not match!"
+        psMsg.className = "text-danger"
+        return false
+    } else {
+        pwMsg.innerText = ""
+        pwMsg.className = ""
+        return true
+    }
+}
 
     window.onload = () => {
         // back to top button
